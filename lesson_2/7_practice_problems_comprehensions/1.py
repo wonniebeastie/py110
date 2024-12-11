34ba8b9b34ffeb4dd6dc02ade0dd2059613c2c22
Wonnie <wonnie.tech@gmail.com>
2024-12-11 11:56:42 -0800

    Move/change some stuff around

--- lesson_2/7_practice_problems_comprehensions/1.py
+++ lesson_2/7_practice_problems_comprehensions/1.py
@@ -12,11 +12,11 @@ O: an integer (the sum of male family members' ages)
 ALGO 
 [x] 1. Create empty "ages to be summed" list.
 [] 2. Loop through each key-value pair of "given dictionary" -
-		[] 2A. Loop through "nested dictionary"
+		[x] 2A. Loop through "nested dictionary"
 			a) If the value of "gender" is "male" - [NOTE: EXTRACTED]
 				Add "age" to "ages to be summed" list.
 			b) Else, go onto next pair.
-[] 3. Repeat until end of dictionary.
+[x] 3. Repeat until end of dictionary.
 [x] 4. Sum the values in "ages to be summed" list.
 [x] 5. Return the sum.
 
@@ -66,17 +66,24 @@ def sum_male_ages(family_info):
 
 print(sum_male_ages(munsters)) # 444
 
-# for name, age_n_gender in family_info.items():
-# 	if age_n_gender['gender'] == 'male':
-# 		ages.append(age_n_gender['age'])
+# Using a comprehension.
+def sum_male_ages_comprehension(family_info):
+	age_list = [person_info['age'] for person_info in family_info.values()
+								if person_info['gender'] == 'male']
+	return sum(age_list)
+
+print(sum_male_ages_comprehension(munsters)) # 444
 
 
-# # Using a comprehension.
-# def sum_male_ages_comprehension(family_info):
-# 	# print(family_info.values())
-# 	# summed_ages = [sum(ages) for ages in family_info.values() if ]
+# SOLUTIONS
+total_male_age = 0
+for member in munsters.values():
+    if member['gender'] == 'male':
+        total_male_age += member['age']
 
-# 	print(summed_ages)
-# 	#return summed_ages
+print(total_male_age)         # 444
 
-# print(sum_male_ages_comprehension(munsters)) # 444
+all_male_ages = [member['age'] for member in munsters.values()
+                            	if member['gender'] == 'male']
+
+print(sum(all_male_ages))     # 444

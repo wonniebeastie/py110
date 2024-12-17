b2e8515398bab8bf2405bee635dca1e5454d0b6e
Wonnie <wonnie.tech@gmail.com>
2024-12-17 12:28:36 -0800

    Convert spaces to tabs

--- lesson_3/tictactoe.py
+++ lesson_3/tictactoe.py
@@ -6,7 +6,7 @@ HUMAN_MARKER = 'X'
 COMPUTER_MARKER = 'O'
 
 def prompt(message):
-    print(f'==> {message}')
+	print(f'==> {message}')
 
 """
 PROBLEM 
@@ -31,7 +31,7 @@ EXP:
 IMP:
 - Empty list as an input produces an empty string as output.
 - The delimiters & different words are optional.
-    - Default is a comma followed by a space & 'or' for the join word.
+	- Default is a comma followed by a space & 'or' for the join word.
 - Just two integers on the list does not display a comma, just 'or'.
 
 
@@ -46,18 +46,18 @@ print(join_or([1, 2]))                  # => "1 or 2"
 
 ALGO [PROGRAMMATIC]
 [x] 1. If "input list" is empty, return an empty string.
-       [NOTE] Function should have default values of:
+	   [NOTE] Function should have default values of:
 			  - ', ' ("delimiter")
 			  - 'or' ("join word")
 [x] 2. If "input list" only has one value, return that value as a string
 [x] 3. If "input list" has exactly two values, return those values 
-      separated by "join word"; don't use "delimiter".
+	  separated by "join word"; don't use "delimiter".
 [x] 4. If "input list" has 3 or more values, return the concatenation of
-      all the values as follows:
+	  all the values as follows:
 		4A) "For each number in 'input list', join each with 'delimiter' 
 		  until the last number; Join the last number with the 'join 
 		  word' to the rest of the numbers.
-		    - [TODO] Two lists for each? Try slicing syntax.
+			- [TODO] Two lists for each? Try slicing syntax.
 				a) from index 0 to index before the last - "first part"
 					a.1. Join each num of "first part" with 'delimiter'
 				b) last index - "second part"
@@ -68,6 +68,11 @@ ALGO [PROGRAMMATIC]
 """
 
 def join_or(num_list, delimiter=', ', join_word='or'):
+	'''
+	Display the numbers (representing squares) that are available to
+	pick from in a neat way, with option to choose a delimiter & a
+	join word.
+	'''
 	match len(num_list):
 		case 0:
 			return ''
@@ -82,51 +87,57 @@ def join_or(num_list, delimiter=', ', join_word='or'):
 	return final_string
 
 def display_board(board):
-    os.system('clear')
-
-    prompt(f"You are {HUMAN_MARKER}. Computer is {COMPUTER_MARKER}.")
-    print('')
-    print('     |     |')
-    print(f"  {board[1]}  |  {board[2]}  |  {board[3]}")
-    print('     |     |')
-    print('-----+-----+-----')
-    print('     |     |')
-    print(f"  {board[4]}  |  {board[5]}  |  {board[6]}")
-    print('     |     |')
-    print('-----+-----+-----')
-    print('     |     |')
-    print(f"  {board[7]}  |  {board[8]}  |  {board[9]}")
-    print('     |     |')
-    print('')
+	'''
+	Display the current state of the board.
+	'''
+	os.system('clear')
+	
+	prompt(f"You are {HUMAN_MARKER}. Computer is {COMPUTER_MARKER}.")
+	print('')
+	print('     |     |')
+	print(f"  {board[1]}  |  {board[2]}  |  {board[3]}")
+	print('     |     |')
+	print('-----+-----+-----')
+	print('     |     |')
+	print(f"  {board[4]}  |  {board[5]}  |  {board[6]}")
+	print('     |     |')
+	print('-----+-----+-----')
+	print('     |     |')
+	print(f"  {board[7]}  |  {board[8]}  |  {board[9]}")
+	print('     |     |')
+	print('')
 
 def initialize_board():
-    return {square: INITIAL_MARKER for square in range(1, 10)}
+	'''
+	Clear the board
+	'''
+	return {square: INITIAL_MARKER for square in range(1, 10)}
 
 def empty_squares(board):
-    # create a list of only the valid choices
-    return [key 
-        	for key, value in board.items() 
-            if value == INITIAL_MARKER]
+	# create a list of only the valid choices
+	return [key 
+			for key, value in board.items() 
+			if value == INITIAL_MARKER]
 
 def player_chooses_square(board):
-    while True:
-        valid_choices = [str(num) for num in empty_squares(board)]
-        # input trimmed to allow spaces in input
-        prompt(f"Choose a square: {join_or(valid_choices)}")
-        square = input().strip()
-        
-        if square in valid_choices:
-            break
+	while True:
+		valid_choices = [str(num) for num in empty_squares(board)]
+		# input trimmed to allow spaces in input
+		prompt(f"Choose a square: {join_or(valid_choices)}")
+		square = input().strip()
+		
+		if square in valid_choices:
+			break
 
-        prompt("Sorry, that's not a valid choice.")
+		prompt("Sorry, that's not a valid choice.")
 
-    board[int(square)] = HUMAN_MARKER
+	board[int(square)] = HUMAN_MARKER
 
 def computer_chooses_square(board):
-    if len(empty_squares(board)) == 0:
-        return
-    square = random.choice(empty_squares(board))
-    board[square] = COMPUTER_MARKER
+	if len(empty_squares(board)) == 0:
+		return
+	square = random.choice(empty_squares(board))
+	board[square] = COMPUTER_MARKER
 
 def board_full(board):
 	'''
@@ -137,27 +148,27 @@ def board_full(board):
 	return len(empty_squares(board)) == 0
 
 def someone_won(board):
-    return bool(detect_winner(board))
+	return bool(detect_winner(board))
 
 def detect_winner(board):
-    winning_lines = [
-        [1, 2, 3], [4, 5, 6], [7, 8, 9],  # rows
-        [1, 4, 7], [2, 5, 8], [3, 6, 9],  # columns
-        [1, 5, 9], [3, 5, 7]              # diagonals
-    ]
+	winning_lines = [
+		[1, 2, 3], [4, 5, 6], [7, 8, 9],  # rows
+		[1, 4, 7], [2, 5, 8], [3, 6, 9],  # columns
+		[1, 5, 9], [3, 5, 7]              # diagonals
+	]
 
-    for line in winning_lines:
-        sq1, sq2, sq3 = line
-        if (board[sq1] == HUMAN_MARKER
-               and board[sq2] == HUMAN_MARKER
-               and board[sq3] == HUMAN_MARKER):
-            return 'Player'
-        elif (board[sq1] == COMPUTER_MARKER
-                  and board[sq2] == COMPUTER_MARKER
-                  and board[sq3] == COMPUTER_MARKER):
-            return 'Computer'
+	for line in winning_lines:
+		sq1, sq2, sq3 = line
+		if (board[sq1] == HUMAN_MARKER
+			   and board[sq2] == HUMAN_MARKER
+			   and board[sq3] == HUMAN_MARKER):
+			return 'Player'
+		elif (board[sq1] == COMPUTER_MARKER
+				  and board[sq2] == COMPUTER_MARKER
+				  and board[sq3] == COMPUTER_MARKER):
+			return 'Computer'
 
-    return None
+	return None
 
 """
 PROBLEM
@@ -226,21 +237,21 @@ ALGO
 	  4A) If `detect_winner` is "Player":
 	  	   - display "Player wins!"
 		   - increment "player score" by 1
-	      If `detect_winner` is "Computer":
+		  If `detect_winner` is "Computer":
 		   - display "Comptuer wins!"
 		   - increment "computer score" by 1
 	  4B) Display both "player score" & "computer score".
 [] 5. Play more rounds until either "player score" or "computer score"
 	  is `5`.
 	  - When either one is 5:
-	      - "Player wins overall match!" if player wins
+		  - "Player wins overall match!" if player wins
 		  - "Computer wins overall match!" if computer wins
 [] 6. "Play another match?"
 	  - If yes:
-	      - reset "player score" & "computer score" to 0
+		  - reset "player score" & "computer score" to 0
 		  - restart from step 1.
 	  - If no, 
-	      - "Thanks for playing Tic Tac Toe!"
+		  - "Thanks for playing Tic Tac Toe!"
 
 `update_scores` ALGO
 [TODO] Reset to `0` for both when restarting a match.
@@ -269,41 +280,41 @@ Algo
 
 """
 def keep_score():
-
+	pass
 
 def play_tic_tac_toe():
-    while True:
-        '''
+	while True:
+		'''
 		Board intialized in outer loop bc each game needs a separate
 		board.
 		'''
-        board = initialize_board()
+		board = initialize_board()
 
-        while True:
-            display_board(board)
+		while True:
+			display_board(board)
 
-            player_chooses_square(board)
-            if someone_won(board) or board_full(board):
-                break
+			player_chooses_square(board)
+			if someone_won(board) or board_full(board):
+				break
 
-            computer_chooses_square(board)
-            if someone_won(board) or board_full(board):
-                break
+			computer_chooses_square(board)
+			if someone_won(board) or board_full(board):
+				break
 
-        display_board(board)
+		display_board(board)
 
-        if someone_won(board):
-            prompt(f"{detect_winner(board)} won!")
-        else:
-            prompt("It's a tie!")
+		if someone_won(board):
+			prompt(f"{detect_winner(board)} won!")
+		else:
+			prompt("It's a tie!")
 		
 		# Ask user if they want to play again.
-        prompt("Play again? (y or n)")
-        answer = input().lower()
+		prompt("Play again? (y or n)")
+		answer = input().lower()
 
-        if answer[0] != 'y':
-            break
+		if answer[0] != 'y':
+			break
 
-    prompt('Thanks for playing Tic Tac Toe!')
+	prompt('Thanks for playing Tic Tac Toe!')
 
 play_tic_tac_toe()

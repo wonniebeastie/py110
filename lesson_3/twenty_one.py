f55e6f3dfd039072494420564579bdd594ee256c
Wonnie <wonnie.tech@gmail.com>
2024-12-31 12:16:40 -0800

    Create function to shuffle cards at the start

--- lesson_3/twenty_one.py
+++ lesson_3/twenty_one.py
@@ -1,3 +1,5 @@
+import random
+
 SUITS = ['H', 'D', 'C', 'S']
 VALUES = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'] 
 
@@ -7,5 +9,6 @@ def initialize_deck():
 						  for value in VALUES]
 	return deck
 
-
+def shuffle(deck):
+	random.shuffle(deck)
 

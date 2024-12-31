4e2eeb3c09991b7ddea107af0135274097d742b9
Wonnie <wonnie.tech@gmail.com>
2024-12-31 12:10:39 -0800

    Create function to initialize entire deck

---
+++ lesson_3/twenty_one.py
@@ -0,0 +1,11 @@
+SUITS = ['H', 'D', 'C', 'S']
+VALUES = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'] 
+
+# Initializes entire deck
+def initialize_deck(): 
+	deck = [[suit, value] for suit in SUITS
+						  for value in VALUES]
+	return deck
+
+
+

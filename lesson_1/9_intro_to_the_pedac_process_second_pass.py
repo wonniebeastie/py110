81839f682d2969b7ea0b8853a678c8cb2ef3a67f
Wonnie <wonnie.tech@gmail.com>
2025-03-07 10:21:33 -0800

    Start working on re-do of intro to pedac process

---
+++ lesson_1/9_intro_to_the_pedac_process_second_pass.py
@@ -0,0 +1,55 @@
+"""
+✱ P - PROBLEM ✱
+
+INPUT & OUTPUT
+I: a string
+O: a string, with all palindromes uppercased
+
+RULES (EXPLICIT/IMPLICIT) / SEQ
+EXP:
+- every paliandrome in the string must be converted to uppercase.
+- a palindrome is a word that reads the same forwards and backwards
+
+IMP:
+- if input string is empty, return an empty string
+- words that are already uppercase remain uppercase
+- palindromes are case sensitive ()
+
+SEQ:
+- 
+
+CLARIFYING QUESTIONS
+- 
+
+MENTAL MODEL
+
+
+✱ E - EXAMPLES/TEST CASES ✱
+
+- Example 1
+- I:
+- O:
+
+- Example 2
+- I:
+- O:
+
+✱ D - DATA STRUCTURE ✱
+
+DS / VIS
+- 
+
+VIS:
+
+
+✱ A - ALGORITHM ✱
+SUB-PROBLEMS
+- 
+
+(-, +, -, +)
+[] - 
+[] - 
+[] -  
+
+"""
+
